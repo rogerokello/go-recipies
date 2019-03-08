7e2fa836933d2d59a974c748426c14d3d555ad73
roger <rogerokello@gmail.com>
2019-03-08 09:47:47 +0300

    Adding comments for short declarative syntax and when to use it

--- type_casting_short_declarative_syntax.go
+++ type_casting_short_declarative_syntax.go
@@ -6,6 +6,8 @@ import (
 )
 
 func main() {
+	// This short form of assignment only works with in functions and only if
+	// we are assigning values to variables at declaration
 	a := 10.0000
 	b := 3
 
