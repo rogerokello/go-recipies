1882717f1f41197b9212eb274607ab7d97c84bcf
roger <rogerokello@gmail.com>
2019-03-10 01:48:03 +0300

    Add code for how to access, modify, insert and delete map values

---
+++ map_access_insert_delete.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	testMap := map[string]int{"A": 1, "B": 2, "C": 3, "D": 4, "E": 5}
+
+	// Accessing a map value
+	fmt.Println("Item at map index A is ", testMap["A"])
+
+	// Printing the whole map
+	fmt.Println("Map has values: ", testMap)
+
+	// Modifying an Existing value
+	testMap["B"] = 100
+	// Adding a non existing value
+	testMap["F"] = 12345
+	fmt.Println("New values for the map: ", testMap)
+
+	// Deleting an element from the map. Use the in built delete method
+	delete(testMap, "C")
+	fmt.Println("Currently existing look of map: ", testMap)
+
+}
