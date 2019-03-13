3fa591e7a3bc8556697113df27ef89c3861f982a
roger <rogerokello@gmail.com>
2019-03-14 00:41:32 +0300

    Use of constants

--- Course 3/GoWorkspace/src/hello.go
+++ Course 3/GoWorkspace/src/hello.go
@@ -2,20 +2,12 @@ package main
 
 import "fmt"
 
-//Salutation  This is a definition of type string to be a Salutation
-// Capitalizing the S in salutation means that salutation will
-// be visible outside this package
-type Salutation struct {
-	name     string
-	greeting string
-}
+const (
+	PI       = 3.14
+	Language = "Go"
+)
 
 func main() {
 
-	var s = Salutation{}
-
-	s.name = "Roger"
-	s.greeting = "Hello"
-
-	fmt.Println(s.greeting, " ", s.name)
+	fmt.Println(PI, Language)
 }
