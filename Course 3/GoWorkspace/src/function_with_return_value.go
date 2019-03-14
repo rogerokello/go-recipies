e71861dc850178077411f3815b1045715f7e071d
roger <rogerokello@gmail.com>
2019-03-14 23:27:11 +0300

    Add comments to verify exported values are capitalised

--- Course 3/GoWorkspace/src/function_with_return_value.go
+++ Course 3/GoWorkspace/src/function_with_return_value.go
@@ -2,15 +2,18 @@ package main
 
 import "fmt"
 
+// Salutation is new type based on a struct
 type Salutation struct {
 	name     string
 	greeting string
 }
 
+// Greet is new type based on a struct
 func Greet(salutation Salutation) {
 	fmt.Println(CreateMessage(salutation.name, salutation.greeting))
 }
 
+// CreateMessage is a way if creating a message on the fly
 func CreateMessage(name, greeting string) string {
 	return greeting + " " + name
 }
