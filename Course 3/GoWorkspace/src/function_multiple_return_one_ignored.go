db9ea93623bf8545da7df40d9bd7ece81ba935fb
roger <rogerokello@gmail.com>
2019-03-15 23:27:51 +0300

    Return multiple values with one ignored

---
+++ Course 3/GoWorkspace/src/function_multiple_return_one_ignored.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// Salutation is new type based on a struct
+type Salutation struct {
+	name     string
+	greeting string
+}
+
+// Greet is new type based on a struct
+func Greet(salutation Salutation) {
+	_, alternate := CreateMessage(salutation.name, salutation.greeting)
+	fmt.Println(alternate)
+}
+
+// CreateMessage is a way if creating a message on the fly
+func CreateMessage(name, greeting string) (string, string) {
+	return greeting + " " + name, "HEY! " + name
+}
+
+func main() {
+	var s = Salutation{"Roger", "Hello"}
+	Greet(s)
+}
