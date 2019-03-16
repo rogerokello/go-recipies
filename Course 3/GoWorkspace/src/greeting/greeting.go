b7eec613ccedf2ce955427ac3c028b90438f8e10
roger <rogerokello@gmail.com>
2019-03-16 12:01:20 +0300

    Create a greeting package contained which if_basic.go will use

--- Course 3/GoWorkspace/src/greeting/greeting.go
+++ Course 3/GoWorkspace/src/greeting/greeting.go
@@ -1,1 +1,36 @@
 package greeting
+
+import "fmt"
+
+// Salutation is new type based on a struct
+type Salutation struct {
+	Name     string
+	Greeting string
+}
+
+// Printer is a type which a function that takes in a string and returns nothing
+type Printer func(string) // or type Printer func(string)()
+
+// Greet takes in a function as a parameter
+func Greet(salutation Salutation, do Printer, isFormal bool) {
+	message, alternate := CreateMessage(salutation.Name, salutation.Greeting)
+	if isFormal {
+		do(message)
+	}
+	do(alternate)
+}
+
+// CreateMessage returns named values
+func CreateMessage(name, greeting string) (message string, alternate string) {
+	message = greeting + " " + name
+	alternate = "HEY! " + name
+
+	return
+}
+
+// MakePrinter is function that returns another function and retains its scope
+func MakePrinter(custom string) Printer {
+	return func(a string) {
+		fmt.Println(a + custom)
+	}
+}
