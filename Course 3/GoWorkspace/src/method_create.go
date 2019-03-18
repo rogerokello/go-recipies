f4f6ffc36a8cac276a79b872d23f24ada39b0b55
roger <rogerokello@gmail.com>
2019-03-18 12:49:24 +0300

    Use Rename pointer method of struct in method_create.go

--- Course 3/GoWorkspace/src/method_create.go
+++ Course 3/GoWorkspace/src/method_create.go
@@ -10,6 +10,8 @@ func main() {
 		{"Mary", "Hi there"},
 	}
 
+	salutations[0].Rename("Peter")
+
 	salutations = salutations.Add(greeting.Salutation{"James", "Oh hi"})
 
 	salutations.Greet(greeting.MakePrinter("!!!!!"), true, 10)
