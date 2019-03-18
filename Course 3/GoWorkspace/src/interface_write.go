f67f081aca898e9393e239d89e1bb9b92decba3e
roger <rogerokello@gmail.com>
2019-03-18 16:43:49 +0300

    Add type that uses writer interface to fmt.Fprintf

---
+++ Course 3/GoWorkspace/src/interface_write.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"./greeting"
+)
+
+func renameToDog(renameInterface greeting.Renamable) {
+	renameInterface.Rename("Dog")
+}
+
+func main() {
+
+	salutations := greeting.Salutations{
+		{"Roger", "Hi"},
+		{"Joe", "Hello"},
+		{"Mary", "Hi there"},
+	}
+
+	// The code below could be written as (&salutations[0]).Rename("Peter")
+	salutations[0].Rename("Peter")
+
+	// We nee to pass in an address to the slice because the Rename method of the
+	// struct takes in a salutation pointer as its first parameter
+	renameToDog(&salutations[0])
+
+	//Use fmt.Fprintf() writer interface
+	// Synopsis
+	// fmt.Fprintf(w io.Writer,a ...interface{}) (n int, err error)
+	fmt.Fprintf(&salutations[0], "Brian number %d", 2)
+
+	salutations = salutations.Add(greeting.Salutation{"James", "Oh hi"})
+
+	salutations.Greet(greeting.MakePrinter("!!!!!"), true, 10)
+}
