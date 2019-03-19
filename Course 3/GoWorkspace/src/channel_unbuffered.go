872cb6d093bbe8b9fde8386f677ac4621e287a24
roger <rogerokello@gmail.com>
2019-03-19 09:04:14 +0300

    Add code for an un_buffered channel that blocks in channel_unbuffered.go

---
+++ Course 3/GoWorkspace/src/channel_unbuffered.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+
+	"./greeting"
+)
+
+func renameToDog(renameInterface greeting.Renamable) {
+	renameInterface.Rename("Dog")
+}
+
+func main() {
+
+	salutations := greeting.Salutations{
+		{"Roger", "Hi"},
+		{"Joe", "Hello"},
+		{"Mary", "Hi there"},
+	}
+
+	// The code below could be written as (&salutations[0]).Rename("Peter")
+	salutations[0].Rename("Peter")
+
+	// We nee to pass in an address to the slice because the Rename method of the
+	// struct takes in a salutation pointer as its first parameter
+	renameToDog(&salutations[0])
+
+	//Use fmt.Fprintf() writer interface
+	// Synopsis
+	// fmt.Fprintf(w io.Writer,a ...interface{}) (n int, err error)
+	fmt.Fprintf(&salutations[0], "Brian number %d", 2)
+
+	salutations = salutations.Add(greeting.Salutation{"James", "Oh hi"})
+
+	done := make(chan bool)
+
+	go func() {
+		salutations.Greet(greeting.MakePrinter(" --Goroutine-- "), true, 10)
+		done <- true
+		done <- true
+		//This line is never reached because the channel is blocked at line 40
+		//and execution continues.
+		fmt.Println("done")
+	}()
+
+	salutations.Greet(greeting.MakePrinter("!!!!!"), true, 10)
+
+	<-done
+}
